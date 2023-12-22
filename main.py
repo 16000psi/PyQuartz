6f7ec7126f78a490bd463b64f32891b802689db4
16000psi <davidjhnsmith@gmail.com>
2023-12-22 17:52:22 +0000

    Add initial sqlite3 database creation

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import argparse
+import sqlite3
 from datetime import datetime
 
 
 class Handler:
     def __init__(self):
-        pass
+        con = sqlite3.connect("timers.db")
+        self.cur = con.cursor()
+        if not self.cur.execute("SELECT name FROM sqlite_master").fetchone():
+            print("databases creating")
+            self.create_timers_table()
 
     def handle(self):
         parser = argparse.ArgumentParser()
         parser.add_argument(
-            "action",
-            choices=["start", "stop"],
-            help="Start or end timer"
+            "action", choices=["start", "stop"], help="Start or end timer"
         )
 
         args = parser.parse_args()
@@ -19,12 +22,31 @@ class Handler:
         action()
 
     def start(self):
-
         print("timer started")
+        breakpoint()
 
     def stop(self):
-
         print("timer stopped")
+
+    def create_timers_table(self):
+        self.cur.execute(
+            """
+            CREATE TABLE timer (
+            timer_id INTEGER PRIMARY KEY,
+            title TEXT
+            );
+            """
+        )
+        self.cur.execute(
+            """
+            CREATE TABLE session (
+                session_id INTEGER PRIMARY KEY,
+                sessiontimer INTEGER REFERENCES timer(timer_id),
+                starttime TEXT,
+                endtime TEXT
+                );
+            """
+        )
 
 
 if __name__ == "__main__":
